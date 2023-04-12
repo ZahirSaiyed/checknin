e18461645c5660024c3adf1010e3b4370ae7edc2
Zahir Saiyed <zahir19saiyed@gmail.com>
2023-04-12 00:14:15 -0400

    Resolving pull conflict

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,73 @@
-import { useState } from 'react';
-import Head from 'next/head';
-import { NextPage } from 'next';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-const Home: NextPage = () => {
-  const [textValue, setTextValue] = useState('');
-  const [numberValue, setNumberValue] = useState<number | null>(null);
-  const [output, setOutput] = useState('');
+interface Response {
+  rating: number;
+  description: string;
+}
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setOutput(`Description: ${textValue}, Rating: ${numberValue}`);
+const Home: React.FC = () => {
+  const [rating, setRating] = useState<number>(1);
+  const [description, setDescription] = useState<string>('');
+  const [responses, setResponses] = useState<Response[]>([]);
+
+  const handleRatingChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setRating(parseFloat(event.target.value));
+  };
+
+  const handleDescriptionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setResponses([...responses, { rating, description }]);
+    setDescription('');
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
-      <Head>
-        <title>CheckNin</title>
-      </Head>
-      <div className="container mx-auto p-4">
-        <h1 className="text-white text-4xl font-bold">CheckNin</h1>
-        <p className="text-white mt-4">
-          How was your day today? Rate your day out of 10
-        </p>
-        <form onSubmit={handleSubmit} className="mt-8">
+    <div>
+      <h1>CheckNin</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="rating">Rate your day (1-10): </label>
           <input
-            className="block w-full bg-white bg-opacity-20 text-white placeholder-white placeholder-opacity-50 border border-white border-opacity-20 rounded p-2 focus:outline-none focus:border-white"
-            type="text"
-            placeholder="Enter text here"
-            value={textValue}
-            onChange={(e) => setTextValue(e.target.value)}
-          />
-          <input
-            className="block w-full bg-white bg-opacity-20 text-white placeholder-white placeholder-opacity-50 border border-white border-opacity-20 rounded p-2 focus:outline-none focus:border-white mt-4"
             type="number"
+            id="rating"
+            name="rating"
+            value={rating}
+            onChange={handleRatingChange}
             min="1"
+            step = "0.1"
             max="10"
-            step="0.01"
-            placeholder="Enter a number between 1 and 10"
-            onChange={(e) => setNumberValue(parseFloat(e.target.value))}
+            style={{color:'black'}}
           />
-          <button
-            className="mt-4 bg-white text-purple-500 font-bold py-2 px-4 rounded hover:bg-opacity-80 transition duration-150 ease-in-out"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-        {output && (
-          <div className="mt-8 bg-white bg-opacity-20 text-white p-4 rounded">
-            <p>{output}</p>
+        </div>
+        <div>
+          <label htmlFor="description">Describe your day:</label>
+          <textarea
+            id="description"
+            name="description"
+            value={description}
+            onChange={handleDescriptionChange}
+            rows={5}
+            cols={40}
+            style={{ resize: 'both', color: 'black', padding: '2px 5px'}}
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <h2>User Responses:</h2>
+        {responses.map((response, index) => (
+          <div key={index} style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '10px', marginBottom: '10px' }}>
+            <p>
+              <strong>Rating:</strong> {response.rating}
+            </p>
+            <p>
+              <strong>Description:</strong> {response.description}
+            </p>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
