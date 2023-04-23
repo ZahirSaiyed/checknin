4606a28a6c0640671322246b486a080c48283793
Zahir Saiyed <zahir19saiyed@gmail.com>
2023-04-23 01:19:32 -0400

    UI changes.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
 
   return (
     <header className="flex justify-between items-center py-4 px-6 bg-white bg-opacity-20">
-      <nav className="flex items-center">
+      <nav className="flex items-center space-x-4">
         <Link href="/" passHref>
           <button className="text-white font-bold hover:underline focus:outline-none">Home</button>
         </Link>
 
         <Link href="/past-checkins" passHref>
-          <button className="ml-4 text-white font-bold hover:underline focus:outline-none">Past Checkins</button>
+          <button className="text-white font-bold hover:underline focus:outline-none">Past Checkins</button>
         </Link>
       </nav>
 
--- src/pages/past-checkins.tsx
+++ src/pages/past-checkins.tsx
@@ -1,3 +1,74 @@
+// import { useEffect, useState } from "react";
+// import { NextPage } from "next";
+// import { useSession } from "next-auth/react";
+// import { OutputData } from "./api/types";
+// import Header from "../components/Header";
+// import RatingChart from "../components/RatingChart";
+// import Link from 'next/link';
+
+// const PastCheckins: NextPage = () => {
+//   const { data: session } = useSession();
+//   const [pastCheckins, setPastCheckins] = useState<OutputData[]>([]);
+
+//   useEffect(() => {
+//     fetchPastCheckins();
+//   }, [session]);
+
+//   const fetchPastCheckins = () => {
+//     if (session) {
+//       fetch("/api/get-checkins", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ userId: session?.user?.email ?? "unknown" }),
+//       })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           if (data.success) {
+//             setPastCheckins(data.checkins);
+//           }
+//         });
+//     }
+//   };
+
+//   return (
+//     <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+//       <Header />
+//       <div className="container mx-auto p-4">
+//         <h1 className="text-white text-4xl font-bold">Past Checkins</h1>
+//         {pastCheckins.length > 0 && (
+//         <div className="my-8">
+//           <RatingChart checkins={pastCheckins} />
+//         </div>
+//       )}
+//         <ul className="mt-4 space-y-4">
+//           {pastCheckins.map((checkin, index) => (
+//             <Link href={`checkin/${checkin._id}`}>
+//             <li
+//               key={index}
+//               className="bg-white bg-opacity-20 text-white p-4 rounded"            >
+//               <div className="flex justify-between items-center">
+//                 <div>
+//                   <p className="text-lg font-semibold">{checkin.text}</p>
+//                   <p className="text-sm">{new Date(checkin.timeStamp).toLocaleDateString()}</p>
+//                 </div>
+//                 <div className="flex items-center">
+//                   <p className="text-xl font-bold mr-2">{checkin.rating}</p>
+//                   <span className="text-sm">/ 10</span>
+//                 </div>
+//               </div>
+//             </li>
+//             </Link>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PastCheckins;
+
 import { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
@@ -36,29 +107,27 @@ const PastCheckins: NextPage = () => {
     <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
       <Header />
       <div className="container mx-auto p-4">
-        <h1 className="text-white text-4xl font-bold">Past Checkins</h1>
+        <h1 className="text-white text-4xl font-bold mb-6">Past Checkins</h1>
         {pastCheckins.length > 0 && (
-        <div className="my-8">
-          <RatingChart checkins={pastCheckins} />
-        </div>
-      )}
+          <div className="my-8">
+            <RatingChart checkins={pastCheckins} />
+          </div>
+        )}
         <ul className="mt-4 space-y-4">
           {pastCheckins.map((checkin, index) => (
-            <Link href={`checkin/${checkin._id}`}>
-            <li
-              key={index}
-              className="bg-white bg-opacity-20 text-white p-4 rounded"            >
-              <div className="flex justify-between items-center">
-                <div>
-                  <p className="text-lg font-semibold">{checkin.text}</p>
-                  <p className="text-sm">{new Date(checkin.timeStamp).toLocaleDateString()}</p>
-                </div>
-                <div className="flex items-center">
-                  <p className="text-xl font-bold mr-2">{checkin.rating}</p>
-                  <span className="text-sm">/ 10</span>
-                </div>
-              </div>
-            </li>
+            <Link href={`checkin/${checkin._id}`} key={index}>
+                <li className="bg-white bg-opacity-20 text-white p-4 rounded hover:bg-opacity-30 cursor-pointer transition duration-150 ease-in-out">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <p className="text-lg font-semibold">{checkin.text}</p>
+                      <p className="text-sm">{new Date(checkin.timeStamp).toLocaleDateString()}</p>
+                    </div>
+                    <div className="flex items-center">
+                      <p className="text-xl font-bold mr-2">{checkin.rating}</p>
+                      <span className="text-sm">/ 10</span>
+                    </div>
+                  </div>
+                </li>
             </Link>
           ))}
         </ul>
