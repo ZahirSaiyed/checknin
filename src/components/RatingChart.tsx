81c48d27b2770520536782b71308c72e3fce76bb
Zahir Saiyed <zahir19saiyed@gmail.com>
2023-04-23 01:48:19 -0400

    Improved chart UI + dropdown fitler uI

--- src/components/RatingChart.tsx
+++ src/components/RatingChart.tsx
@@ -1,95 +1,3 @@
-// import { Line } from "react-chartjs-2";
-// import { InputData } from "../pages/api/types";
-// import { Chart, LinearScale } from 'chart.js';
-// import { CategoryScale, LineElement } from 'chart.js/auto';
-
-// Chart.register(CategoryScale, LinearScale, LineElement);
-
-// interface RatingChartProps {
-//   checkins: InputData[];
-// }
-
-// const RatingChart: React.FC<RatingChartProps> = ({ checkins }) => {
-//   const data = {
-//     labels: checkins.map((checkin) => new Date(checkin.timeStamp).toLocaleDateString()).reverse(),
-//     datasets: [
-//       {
-//         label: "Rating",
-//         data: checkins.map((checkin) => checkin.rating).reverse(),
-//         fill: false,
-//         backgroundColor: "rgba(255, 255, 255, 0.8)",
-//         borderColor: "rgba(255, 255, 255, 0.8)",
-//         tension: 0.4,
-//       },
-//     ],
-//   };
-
-//   const options = {
-//     scales: {
-//       x: {
-//         ticks: {
-//           color: "#ffffff",
-//           reverse: true,
-//         },
-//         grid: {
-//           display: false,
-//         },
-//       },
-//       y: {
-//         ticks: {
-//           color: "#ffffff",
-//         },
-//         grid: {
-//           display: false
-//         },
-//         beginAtZero: true,
-//         max: 10,
-//       },
-//     },
-//     plugins: {
-//       legend: {
-//         display: false,
-//       },
-//       title: {
-//         display: true,
-//         text: 'Ratings Over Time',
-//         font: {
-//           size: 24,
-//           weight: 'bold',
-//           family: "'Inter', sans-serif"
-//         },
-//         color: "#ffffff",
-//         padding: {
-//           top: 20,
-//           bottom: 10
-//         }
-//       }
-//     },
-//     responsive: true,
-//     maintainAspectRatio: true,
-//     interaction: {
-//       intersect: false,
-//       mode: "index",
-//     },
-//     pointHoverRadius: 8,
-//     pointHoverBackgroundColor: "#ffffff",
-//     pointHoverBorderColor: "#ffffff",
-//     pointHoverBorderWidth: 2,
-//     pointHitRadius: 10,
-//   };
-
-//   return (
-//     <div className="p-4">
-//       <div className="bg-black rounded-lg shadow-md p-6" style={{ height: "400px" }}>
-//         <Line data={data} options={options} />
-//       </div>
-//     </div>
-//   );
-
-// };
-
-// export default RatingChart;
-
 import { Line } from "react-chartjs-2";
 import { InputData } from "../pages/api/types";
 import { Chart, LinearScale } from 'chart.js';
@@ -120,19 +28,21 @@ const RatingChart: React.FC<RatingChartProps> = ({ checkins }) => {
     scales: {
       x: {
         ticks: {
-          color: "#ffffff",
+          color: "rgba(255, 255, 255, 0.7)",
           reverse: true,
         },
         grid: {
-          display: false,
+          display: true,
+          color: "rgba(255, 255, 255, 0.1)",
         },
       },
       y: {
         ticks: {
-          color: "#ffffff",
+          color: "rgba(255, 255, 255, 0.7)",
         },
         grid: {
-          display: false
+          display: true,
+          color: "rgba(255, 255, 255, 0.1)",
         },
         beginAtZero: true,
         max: 10,
@@ -158,7 +68,8 @@ const RatingChart: React.FC<RatingChartProps> = ({ checkins }) => {
       }
     },
     responsive: true,
-    maintainAspectRatio: true,
+    maintainAspectRatio: false,
+    aspectRatio: 2,
     interaction: {
       intersect: false,
       mode: "index",
@@ -172,11 +83,11 @@ const RatingChart: React.FC<RatingChartProps> = ({ checkins }) => {
 
   return (
     <div className="p-4">
-      <div className="bg-black rounded-lg shadow-md p-6" style={{ height: "400px" }}>
+      <div className="bg-black rounded-lg shadow-md p-6 h-auto">
         <Line data={data} options={options} />
       </div>
     </div>
-  );
+  );  
 };
 
 export default RatingChart;
--- src/pages/past-checkins.tsx
+++ src/pages/past-checkins.tsx
@@ -6,6 +6,27 @@ import Header from "../components/Header";
 import RatingChart from "../components/RatingChart";
 import Link from 'next/link';
 
+const FilterDropdown: React.FC<{ onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void }> = ({ onChange }) => {
+  return (
+    <div className="relative w-56">
+      <select
+        className="block appearance-none w-full bg-white bg-opacity-20 text-white py-2 pr-8 pl-4 rounded-lg leading-tight focus:outline-none"
+        onChange={onChange}
+      >
+        <option value="date-desc">Date (Newest First)</option>
+        <option value="date-asc">Date (Oldest First)</option>
+        <option value="rating-desc">Rating (Highest First)</option>
+        <option value="rating-asc">Rating (Lowest First)</option>
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+        <svg className="fill-current h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M0 6l10 10 10-10z" />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
 const PastCheckins: NextPage = () => {
   const { data: session } = useSession();
   const [pastCheckins, setPastCheckins] = useState<OutputData[]>([]);
@@ -56,21 +77,11 @@ const PastCheckins: NextPage = () => {
       <div className="container mx-auto p-4">
         <h1 className="text-white text-4xl font-bold mb-6">Past Checkins</h1>
         <div className="flex justify-end mb-4">
-          <label htmlFor="filter" className="text-white mr-2">Filter by:</label>
-          <select
-            name="filter"
-            id="filter"
-            className="rounded-lg bg-white bg-opacity-20 text-white px-2 py-1"
-            onChange={handleFilterChange}
-          >
-            <option value="date-desc">Date (Newest First)</option>
-            <option value="date-asc">Date (Oldest First)</option>
-            <option value="rating-desc">Rating (Highest First)</option>
-            <option value="rating-asc">Rating (Lowest First)</option>
-          </select>
-        </div>
+  <label htmlFor="filter" className="text-white mr-2">Filter by:</label>
+  <FilterDropdown onChange={handleFilterChange} />
+</div>
         {pastCheckins.length > 0 && (
-          <div className="my-8">
+          <div className="my-8 flex-grow">
             <RatingChart checkins={pastCheckins} />
           </div>
         )}
