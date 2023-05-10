7ee2fbcb408e64045fb0951974c4e184c33cb954
Jeralan <jeremyaangel@gmail.com>
2023-05-10 14:22:00 -0500

    Fixed auto add member upon post Allows for easy adding to their pods page and notification propogation

--- src/pages/api/add-pod.ts
+++ src/pages/api/add-pod.ts
@@ -9,12 +9,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       const client = await clientPromise;
       const collection = client.db("checkins").collection("pods");
-      const podMembers = (await collection.find({ _id: new ObjectId(pod) }).toArray())[0].shared ?? [pod.userId]
-      if (!podMembers.contains(userId)) {
+      const podL = (await collection.find({ _id: new ObjectId(pod) }).toArray())
+      const podMembers = podL[0].shared ?? [pod.userId]
+      if (!podMembers.includes(userId)) {
         podMembers.push(userId)
       }
-      console.log("HI",podMembers)
-      const result = await collection.updateMany({userId }, {$set: { shared: podMembers}})
+      const result = await collection.updateMany({ _id: new ObjectId(pod) }, {$set: { shared: podMembers}})
       res.status(200).json({ success: result.acknowledged })
     } catch (error) {
       res.status(500).json({ success: false, message: (error as Error).message });
